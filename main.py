b985a73566d30dc96e3ca2f99dda6384052cc831
“[Iman <[iman.milo@yahoo.com]>
2021-09-30 15:01:07 +0200

    added two lines

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+#This is a line that Iman has added
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +14,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Iman2')
+    print_hi('Iman20')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
